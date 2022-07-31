bdb7ce74dc4d86cc72671bbf8af5ca450e9a7fb6
823984418 <823984418@qq.com>
2022-07-31 18:07:31 +0800

    注释

--- near_map.c
+++ near_map.c
@@ -9,28 +9,25 @@
 
 #define MIN_DELTA   0.0001f
 
-static void circle_shadow300(float32_2_t coord, float32_2_t *shadow, float32_t *dest) {
+static float32_t circle_shadow300(float32_2_t coord, float32_2_t *shadow) {
     float32_t len = sqrtf(coord.x * coord.x + coord.y * coord.y + MIN_DELTA);
     float32_t inv = 300.0f / len;
     *shadow = (float32_2_t) {coord.x * inv, coord.y * inv};
-    if (dest) {
-        *dest = len;
-    }
+    return len;
 }
 
-static bool ds2022_map_nearest(const struct near_map *self, float32_2_t coord, float32_2_t *near) {
+static bool ds2022_map_nearest(const near_map_t *self, float32_2_t coord, float32_2_t *near) {
     float32_t x = coord.x;
     float32_t y = coord.y;
     float32_2_t shadow;
-    float32_t len;
     if (x < 300) {
         if (y < 300) {
-            circle_shadow300((float32_2_t) {x - 300, y - 300}, &shadow, NULL);
+            circle_shadow300((float32_2_t) {x - 300, y - 300}, &shadow);
             *near = (float32_2_t) {shadow.x + 300, shadow.y + 300};
         } else if (y < 900) {
             *near = (float32_2_t) {0, y};
         } else {
-            circle_shadow300((float32_2_t) {x - 300, y - 900}, &shadow, NULL);
+            circle_shadow300((float32_2_t) {x - 300, y - 900}, &shadow);
             *near = (float32_2_t) {shadow.x + 300, shadow.y + 900};
         }
     } else if (x < 1200) {
@@ -41,30 +38,32 @@ static bool ds2022_map_nearest(const struct near_map *self, float32_2_t coord, f
         }
     } else if (x < 1650) {
         if (y < 300) {
-            circle_shadow300((float32_2_t) {x - 1200, y - 300}, &shadow, &len);
+            float32_t len = circle_shadow300((float32_2_t) {x - 1200, y - 300}, &shadow);
             if (len - 300 > y) {
                 *near = (float32_2_t) {x, 0};
             }
             *near = (float32_2_t) {shadow.x + 1200, shadow.y + 300};
+            // 避免在这里校准
             return true;
         } else if (y < 900) {
             *near = (float32_2_t) {1500, y};
         } else {
-            circle_shadow300((float32_2_t) {x - 1200, y - 300}, &shadow, &len);
+            float32_t len = circle_shadow300((float32_2_t) {x - 1200, y - 300}, &shadow);
             if (len - 300 > 1200 - y) {
                 *near = (float32_2_t) {x, 1200};
             }
             *near = (float32_2_t) {shadow.x + 1200, shadow.y + 900};
+            // 避免在这里校准
             return true;
         }
     } else {
         if (y < 300) {
-            circle_shadow300((float32_2_t) {x - 1500, y - 300}, &shadow, NULL);
+            circle_shadow300((float32_2_t) {x - 1500, y - 300}, &shadow);
             *near = (float32_2_t) {shadow.x + 1500, shadow.y + 300};
         } else if (y < 900) {
             *near = (float32_2_t) {1800, y};
         } else {
-            circle_shadow300((float32_2_t) {x - 1500, y - 900}, &shadow, NULL);
+            circle_shadow300((float32_2_t) {x - 1500, y - 900}, &shadow);
             *near = (float32_2_t) {shadow.x + 1500, shadow.y + 900};
         }
     }
